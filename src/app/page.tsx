71363d846aedf6a3821359b3e1367112d2aff21d
Colin Friedlander <cfriedlanderr@gmail.com>
2023-09-11 16:22:41 -0400

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1 className="flex justify-center items-center w-full h-full text-5xl font-bold">Chat Application</h1>
+    </div>
+  )
+}
+
+export default App
